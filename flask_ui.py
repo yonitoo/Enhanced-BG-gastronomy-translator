32ca3635228fb40cb296ef981becd89c7d4f7060
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2024-02-13 04:15:24 +0200

    show the query in the UI

--- flask_ui.py
+++ flask_ui.py
@@ -1,7 +1,20 @@
+import os
+
+import deepl
+from bulstem.stem import BulStemmer
 from flask import Flask, request, render_template_string
 
-from enhanced_translator import translate, postprocess_translation
+from enhanced_translator_stem import translate, postprocess_translation
 
+# stemmer = BulStemmer.from_file('./bulstem_stemming_rules/stem_rules_context_2_utf8.txt',
+#                                min_freq=2, left_context=2)
+# auth_key = os.getenv("DEEPL_AUTH_KEY")
+# if not auth_key:
+#     raise ValueError("DEEPL_AUTH_KEY не е зададен като променлива на средата.")
+# translator = deepl.Translator(auth_key)
+# target_lang = "EN-GB"
+# tag_handling = "html"
+# do_not_translate_terms_json_filename = "Glossary_cleaned.json"
 app = Flask(__name__)
 
 TEMPLATE = '''
@@ -15,6 +28,10 @@ TEMPLATE = '''
             <input type="text" name="query" placeholder="Въведете текст за превод:" style="width: 300px;"/>
             <input type="submit" value="Translate" />
         </form>
+        {% if query %}
+            <h3>Заявка:</h3>
+            <p>{{ query | safe }}</p>
+        {% endif %}
         {% if translation %}
             <h3>Превод:</h3>
             <p>{{ translation | safe }}</p>
@@ -23,7 +40,7 @@ TEMPLATE = '''
             <h3>Разяснения:</h3>
             <ol>
             {% for footnote in footnotes | reverse %}
-                ({{ footnote[0] }}) {{ footnote[1] | safe }}<br>
+                ({{ footnote[0] | safe }}) {{ footnote[1] | safe }}<br>
             {% endfor %}
             </ol>
         {% endif %}
@@ -34,11 +51,7 @@ TEMPLATE = '''
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    """
-    Семпъл потребителски интерфейс за приложението.
-
-    :return: рендерира шаблона за уеб страницата
-    """
+    query = ""
     translation = ""
     footnotes = []
     if request.method == 'POST':
@@ -47,7 +60,7 @@ def home():
         translation = postprocess_translation(translation)
         for footnote in footnotes:
             postprocess_translation(footnote[1])
-    return render_template_string(TEMPLATE, translation=translation, footnotes=footnotes)
+    return render_template_string(TEMPLATE, query=query, translation=translation, footnotes=footnotes)
 
 
 if __name__ == '__main__':
